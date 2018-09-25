01617acd33a35f64a0fa64b6f5c9cc4559926b08
Billy <billy.ward@hotmail.com>
2018-09-25 15:08:53 +0300

    Tutorial part 2 challenge implemented. URL /users/cool now returns a select piece of text.

--- express-locallibrary-tutorial/routes/users.js
+++ express-locallibrary-tutorial/routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/*  Tutorial Part 2 Challenge
+    Returns a piece of text at the URL '/users/cool'
+*/
+router.get('/cool', function(req, res, next) {
+  res.send("You're so cool");
+});
+
 module.exports = router;
