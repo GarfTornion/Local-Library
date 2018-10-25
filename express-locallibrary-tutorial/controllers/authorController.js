ed042f247626c64a54fefdca1ed93a890e4972c6
Billy <billy.ward@hotmail.com>
2018-10-25 21:21:56 +0300

    Added skeleton-callbacks related to author creation into authorController.js.

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -9,4 +9,14 @@ exports.author_list = function(req, res) {
 //Display detail page for a specific Author.
 exports.author_detail = function(req, res) {
     res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+};
+
+//Display Author create form on GET.
+exports.author_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author create GET');
 }
+
+//Handle Author create on POST.
+exports.author_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author create POST');
+};
