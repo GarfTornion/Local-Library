81d726b59e376b3b4beb873394fde41acf7b7d00
Billy <billy.ward@hotmail.com>
2018-10-25 21:18:29 +0300

    Created files for controllers. Added callback-functions for diplaying authors into authorController.js.

---
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -0,0 +1,12 @@
+//Load the module containing the AuthorSchema
+var Author = require('../models/author');
+
+//Display list of all Authors.
+exports.author_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author list');
+};
+
+//Display detail page for a specific Author.
+exports.author_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+}
